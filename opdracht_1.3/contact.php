3abbd69154eaffdaf19a4b5843a33af0248c4e19
Caedendi <bartcommandeur23@gmail.com>
2019-06-06 11:35:59 +0200

    1.3 updated form for input and error messages
    - input field information processes correctly and is checked for
    security purposes
    - error message provided if a required field is empty
    - if all information is valid, it is displayed below the form
    - CSS updated to work with new code
    - commented all new code

--- opdracht_1.3/contact.php
+++ opdracht_1.3/contact.php
@@ -1,10 +1,123 @@
-
-
+<!DOCTYPE html>
 <html>
+  <head>
+    <title>Opdracht 1.3 Contact</title>
+    <link rel="stylesheet" type="text/css" href="./css/FirstExternalSheet.css">
+  </head>
+
   <body>
 
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?>
+    <?php
+    // define variables and set to empty values
+    $nameError = $emailError = $messageError = "";
+    $name = $email = $message = "";
 
+    // processes information provided in input fields.
+    // if field is empty, an error message is made.
+    // else it is tested for security purposes and stored
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      // processes name
+      if (empty($_POST["name"])) {
+        $nameError = "Please provide a valid name";
+      } else {
+        $name = test_input($_POST["name"]);
+      }
+
+      // processes email address
+      if (empty($_POST["email"])) {
+        $emailError = "Please provide a valid email address";
+      } else {
+        $email = test_input($_POST["email"]);
+      }
+
+      // processes message
+      if (empty($_POST["message"])) {
+        $messageError = "Please type your message";
+      } else {
+        $message = test_input($_POST["message"]);
+      }
+    }
+
+    // tests form input data for security purposes
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    ?>
+
+    <!-- header -->
+    <h1>Contact</h1>
+
+    <!-- navigation bar -->
+    <div class="navbar">
+      <ul>
+       <li><a href="index.html">HOME</a></li>
+       <li><a href="about.html">ABOUT</a></li>
+       <li><a class="active" href="contact.php">CONTACT</a></li>
+      </ul>
+    </div>
+
+    <!-- main site area -->
+    <div class="mainBody">
+       <p>Om contact met mij op te nemen, gelieve het onderstaande formulier in te vullen.</p>
+
+       <!-- formfield -->
+       <div class="formfield">
+         <p class="required">* Required field</p>
+         <!-- displays error messages above form input fields if applicable -->
+         <p class="errormessage">
+           <?php
+             if (!empty($nameError)) { echo $nameError, "<br>"; }
+             if (!empty($emailError)) { echo $emailError, "<br>"; }
+             if (!empty($messageError)) { echo $messageError, "<br>"; }
+           ?>
+         </p>
+         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+           <!-- name -->
+           <div class="formrow">
+               <label for="naam">Naam:</label>
+               <input type="text" name="name" placeholder="uw volledige naam">
+               <span> * </span>
+           </div>
+           <!-- email -->
+           <div class="formrow">
+               <label for="email">Email:</label>
+               <input type="email" name="email" placeholder="uw e-mailadres">
+               <span> * </span>
+           </div>
+           <!-- message -->
+           <div class="formrow">
+               <label for="bericht">Bericht:</label>
+               <textarea name="message" placeholder="typ hier uw bericht" rows="10" cols="50"></textarea>
+               <span> * </span>
+           </div>
+           <!-- submit button -->
+           <div class="formrow">
+               <label for="submit"></label> <!-- empty label -->
+               <input type="submit" value="Verstuur">
+          </div>
+         </form>
+       </div> <!-- end formfield -->
+
+       <!-- Shows all entered input if input is correct -->
+       <?php
+       if (!empty($name) && !empty($email) && !empty($message)) {
+         echo $name;
+         echo "<br><br>";
+         echo $email;
+         echo "<br><br>";
+         echo $message;
+         echo "<br><br>";
+       }
+       ?>
+    </div>
+
+    <!-- footer area -->
+    <footer>
+      <p>&copy; 2019 Bart Commandeur</p>
+    </footer>
   </body>
 </html>
