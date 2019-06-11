a5c5f7c9e077a14568843c6ae2582870ad49795d
Caedendi <bartcommandeur23@gmail.com>
2019-06-11 16:09:29 +0200

    rearranged index loading order

--- opdracht_2.1/index.php
+++ opdracht_2.1/index.php
@@ -4,12 +4,11 @@
   function getRequestedPage() {
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
       isset($_GET['page']) ? $page = $_GET['page'] : $page = 'home';
-      return $page;
     }
     else if ($_SERVER["REQUEST_METHOD"] == "POST") {
       isset($_POST['page']) ? $page = $_POST['page'] : $page = 'home';
-      return $page;
     }
+    return $page;
   }
 
   function showMainContent($page) {
@@ -39,17 +38,17 @@
   function showBodySection($page) {
     include 'body_start.php'; showBodyStart();
     include 'navbar.php'; showMenu($page);
+
+    // header voor elke pagina inbouwen
     showMainContent($page);
+    include 'footer.php'; showFooter();
+    include 'body_end.php'; showBodyEnd();
   }
 
   function showResponsePage($page) {
     include 'html_start.php'; showStartHtml();
-
-    // header voor elke pagina inbouwen
     include 'head_section.php'; showHeadSection();
     showBodySection($page);
-    include 'footer.php'; showFooter();
-    include 'body_end.php'; showBodyEnd();
     include 'html_end.php'; showHtmlEnd();
   }
 
