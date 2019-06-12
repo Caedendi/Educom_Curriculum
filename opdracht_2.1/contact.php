048670630924e46c8d4e218618a7d8911d343a7e
Caedendi <bartcommandeur23@gmail.com>
2019-06-12 10:45:00 +0200

    WIP: re-formatting contact.php
    Rewrote functions according to assignment example/PSD.
    Repositioned code.
    No errors, but no formfield on GET.
    POST not tested yet.
    
    TODO:
    - show empty formfield on GET,
    - then test POST
    - make new page: thanks for submitting

--- opdracht_2.1/contact.php
+++ opdracht_2.1/contact.php
@@ -1,132 +1,125 @@
 <?php
+
+function validateContactForm() {
+  $name = getPostVar('name');
+  $email = getPostVar('email');
+  $message = getPostVar('message');
+  $valid = (!empty($name) && !empty($email) && !empty($message));
+  return $valid;
+}
+
+// tests form input data for security purposes
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
 function showContactContent() {
-
-  // business logic en show logic opdelen in functies
-  // en derde functie validatie
-
-  // ================================
-  // business logic
-  // ================================
-
   // define variables and set to empty values
   $nameError = $emailError = $messageError = "";
   $name = $email = $message = "";
+  $valid = "";
 
-  // tests form input data for security purposes
-  function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
+  $requestType = $_SERVER["REQUEST_METHOD"];
+  if ($requestType == "POST") {
+    // validate contactform
 
-  // processes information provided in input fields.
-  // if field is empty, an error message is made.
-  // else it is tested for security purposes and stored
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $valid = validateContactForm();
+    if($valid) {
 
-    // processes name
-    if (empty($_POST["name"])) {
-      $nameError = "Please provide a valid name";
-    } else {
-      $name = test_input($_POST["name"]);
-    }
+        // valid? show thanks + submitted info
 
-    // processes email address
-    if (empty($_POST["email"])) {
-      $emailError = "Please provide a valid email address";
-    } else {
-      $email = test_input($_POST["email"]);
-    }
-
-    // processes message
-    if (empty($_POST["message"])) {
-      $messageError = "Please type your message";
-    } else {
-      $message = test_input($_POST["message"]);
-    }
-  }
-
-// ================================
-// show logic
-// ================================
-
-echo '
-    <!-- main site area -->
-    <div class="mainBody">
-      <!-- header -->
-      <h1 class="header">Contact</h1>
-
-      <p>Om contact met mij op te nemen, gelieve het onderstaande formulier in te vullen.</p>
-
-      <!-- formfield -->
-      <div class="formField">
-        <p class="required">* Required field</p>
-        <!-- displays error messages above form input fields if applicable -->
-        <p class="errorMessage">
+        echo '
+          // <!-- Shows all entered input if input is correct -->
+          <div class="afterForm">
+            echo "<br><br>";
+            echo "Bedankt voor uw bericht. Er zal zo snel mogelijk contact met u worden opgenomen.";
+            echo "<br><br><br>";
+            echo "Uw verstuurde gegevens: <br><br>";
+            echo "Naam: " . $name;
+            echo "<br>";
+            echo "E-mail: " . $email;
+            echo "<br>";
+            echo "Bericht: ". $message;
+            echo "<br>";
+          </div>
           ';
-        //  if (!empty($nameError)) { echo $nameError; }
-        //  if (!empty($emailError)) { echo $emailError; }
-        //  if (!empty($messageError)) { echo $messageError; }
-        echo '
-        </p>
-        <form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">
-          <input type="hidden" name="page" value="contact"> <!-- to redirect back to contact page instead of home -->
-          <!-- name -->
-          <div class="formRow">
-            <label for="name">Naam:</label>
-            <input type="text" name="name" id="name" placeholder="uw volledige naam" value="'.$name.'">
-            <span class="required"> * '.$nameError.'</span>
-          </div>
-          <!-- email -->
-          <div class="formRow">
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" placeholder="uw e-mailadres" value="'.$email.'">
-            <span class="required"> * '.$emailError.'</span>
-          </div>
-          <!-- message -->
-          <div class="formRow">
-            <label for="message">Bericht:</label>
-            <textarea name="message" id="message" placeholder="typ hier uw bericht" rows="10" cols="50">';
-              if(isset($message)) { echo $message; } echo '</textarea> <!-- remember text inside text area -->
-              <span class="required"> * '.$messageError.'</span>
-          </div>
-          <!-- submit button -->
-          <div class="formRow">
-            <label for="submit"></label> <!-- empty label -->
-            <input type="submit" value="Verstuur">
-          </div>
-        </form>
-      </div> <!-- end formfield -->
+    }
+    else { // else show contact form (partly filled)
 
-      <!-- Shows all entered input if input is correct -->
-      <div class="belowForm">
-        ';
-        if (!empty($name) && !empty($email) && !empty($message)) {
-          echo "<br><br>";
-          echo "Bedankt voor uw bericht. Er zal zo snel mogelijk contact met u worden opgenomen.";
-          echo "<br><br><br>";
-          echo "Uw verstuurde gegevens: <br><br>";
-          echo "Naam: " . $name;
-          echo "<br>";
-          echo "E-mail: " . $email;
-          echo "<br>";
-          echo "Bericht: ". $message;
-          echo "<br>";
-        }
-        echo '
-      </div>
-    </div>
+      $name = test_input(getPostVar('name'));
+      if (empty($name)) {
+        $nameError = "Please provide a valid name";
+      }
 
-<!--
-      // =============================================
-      // PSD
-      // ValidateInput
-      // Valid ? show thanks : show contactform
-      // =============================================
--->
+      $email = test_input(getPostVar('email'));
+      if (empty($email)) {
+        $emailError = "Please provide a valid email address";
+      }
 
-  </body>
-  ';
+      $message = test_input(getPostVar('message'));
+      if (empty($message)) {
+        $messageError = "Please type your message";
+      }
+
+      echo '
+        <!-- main site area -->
+        <div class="mainBody"
+          <p>Om contact met mij op te nemen, gelieve het onderstaande formulier in te vullen.</p>
+
+          <!-- formfield -->
+          <div class="formField">
+            <p class="required">* Required field</p>
+            <!-- displays error messages above form input fields if applicable -->
+            <p class="errorMessage">
+              ';
+            //  if (!empty($nameError)) { echo $nameError; }
+            //  if (!empty($emailError)) { echo $emailError; }
+            //  if (!empty($messageError)) { echo $messageError; }
+            echo '
+            </p>
+            <form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">
+              <input type="hidden" name="page" value="contact"> <!-- to redirect back to contact page instead of home -->
+              <!-- name -->
+              <div class="formRow">
+                <label for="name">Naam:</label>
+                <input type="text" name="name" id="name" placeholder="uw volledige naam" value="'.$name.'">
+                <span class="required"> * '.$nameError.'</span>
+              </div>
+              <!-- email -->
+              <div class="formRow">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" placeholder="uw e-mailadres" value="'.$email.'">
+                <span class="required"> * '.$emailError.'</span>
+              </div>
+              <!-- message -->
+              <div class="formRow">
+                <label for="message">Bericht:</label>
+                <textarea name="message" id="message" placeholder="typ hier uw bericht" rows="10" cols="50">';
+                  if(isset($message)) { echo $message; } echo '</textarea> <!-- remember text inside text area -->
+                  <span class="required"> * '.$messageError.'</span>
+              </div>
+              <!-- submit button -->
+              <div class="formRow">
+                <label for="submit"></label> <!-- empty label -->
+                <input type="submit" value="Verstuur">
+              </div>
+            </form>
+          </div> <!-- end formfield -->
+        </div> // end mainBody
+      ';
+    }
+  }
+  else { // if GET
+    // show contact form (empty)
+    echo 'show form';
+
+
+
+
+
+  }
 }
 ?>
