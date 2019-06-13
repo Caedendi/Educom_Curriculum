5231c622a8a77c02ff69d36ab04f2a7905153693
Caedendi <bartcommandeur23@gmail.com>
2019-06-13 15:48:25 +0200

    WIP: userdata bestanden aangemaakt

---
+++ opdracht_2.2/users/userdata_management.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function authorizeUser($email, $password) {
+
+}
+
+function isEmailKnown($email) {
+
+}
+
+function storeUser($name, $email, $password) {
+  
+}
+
+
+
+
+?>
---
+++ opdracht_2.2/users/userdata_source.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+function findUserByEmail($email) {
+
+}
+
+function saveUser($name, $email, $password) {
+
+}
+
+function deleteUser($email) {
+  
+}
+
+
+?>
