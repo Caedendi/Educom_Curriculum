376fa6760803a4fcf40bdbe37a460fabf501780f
Caedendi <bartcommandeur23@gmail.com>
2019-06-13 15:56:29 +0200

    userdata_management spelling mistake

--- opdracht_2.2/users/userdata_management.php
+++ opdracht_2.2/users/userdata_management.php
@@ -1,7 +1,7 @@
 <?php
 
 
-function authorizeUser($email, $password) {
+function authoriseUser($email, $password) {
 
 }
 
@@ -10,7 +10,7 @@ function isEmailKnown($email) {
 }
 
 function storeUser($name, $email, $password) {
-  
+
 }
 
 
