33ac0f12a6768eb0eabee567aa31afd5b7472bb9
Caedendi <bartcommandeur23@gmail.com>
2019-06-14 15:28:00 +0200

    added to do list

--- opdracht_2.2/header.php
+++ opdracht_2.2/header.php
@@ -3,5 +3,8 @@ function showHeader($page) {
   echo '
     <h1 class="header">'. ucfirst($page) . '</h1>
   ';
+
+  /* JH: Omdat $page nooit door test_input is gehaald, is deze code vatbaar voor XSS (cross side scripting) */
+  
 }
 ?>
