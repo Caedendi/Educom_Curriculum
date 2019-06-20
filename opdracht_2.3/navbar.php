3f9da19252e51291bee04b4e6e1e073abfdd4d03
Caedendi <bartcommandeur23@gmail.com>
2019-06-20 11:42:51 +0200

    navbar first name bugfix

--- opdracht_2.3/navbar.php
+++ opdracht_2.3/navbar.php
@@ -1,19 +1,12 @@
 <?php
-
-// button logout krijgt tekst: Logout [naam]
-// logout alleen laten zien wanneer ingelogd
-// login/register alleen laten zien wanneer niet ingelogd
-
 function showMenu($page) {
   showMenuStart();
   showMenuItem('home', "HOME", "regularPage", $page);
   showMenuItem('about', "ABOUT", "regularPage", $page);
   showMenuItem('contact', "CONTACT", "regularPage", $page);
   if (DEBUG_TEST_PAGE) { showMenuItem('debug', "DEBUG", "regularPage", $page); }
-  /* JH TIP: Laat alle interactie met $_SESSION lopen via 1 php file, bijv. session_manager.php met functies als: loginUser($name, $email), isUserLoggedIn(), getLoggedInUserName(), logoutUser() etc */
-  if (isset($_SESSION['user'])) {
-    $firstName = substr($_SESSION['user_name'], 0, strpos($_SESSION['user_name'], " ")); /* JH TIP: Dit kan korter met explode(' ', $_SESSION['userName'])[0] */
-    showMenuItem('logout', "LOGOUT [" . $firstName . "]", "logout", $page);
+  if (isUserLoggedIn()) {
+    showMenuItem('logout', "LOGOUT [" . explode(' ', $_SESSION['userName'])[0] . "]", "logout", $page); // shows logged in user's first name on logout button
   }
   else {
     showMenuItem('login', "LOGIN", "login", $page);
