e441462a855f7676386d80f68f118ecf30d6af4b
Caedendi <bartcommandeur23@gmail.com>
2019-06-20 11:43:13 +0200

    function rename

--- opdracht_2.3/pages/contact_thanks.php
+++ opdracht_2.3/pages/contact_thanks.php
@@ -4,9 +4,9 @@ function showThanksContent($data) {
     <!-- Shows all entered input if input is correct -->
     <p class="thanksMessage">Bedankt voor uw bericht. Er zal zo spoedig mogelijk contact met u worden opgenomen.</p>
     <p class="thanksMessage">Uw verstuurde gegevens:<br>
-      Naam: ' . getArrayVar($data, "name") . '<br>
-      E-mail: ' . getArrayVar($data, "email") . '<br>
-      Bericht: ' . getArrayVar($data, "message") . '
+      Naam: ' . getArrayValue($data, "name") . '<br>
+      E-mail: ' . getArrayValue($data, "email") . '<br>
+      Bericht: ' . getArrayValue($data, "message") . '
   ';
 }
 ?>
