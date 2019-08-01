1923402d596b9013189f5f690d727c3902fecf55
Caedendi <bartcommandeur23@gmail.com>
2019-08-01 16:47:43 +0200

    undo mergetest

--- opdracht_3.1_mergetest/pages/error_technical.php
+++
@@ -1,8 +0,0 @@
-<?php
-function showErrorTechnicalContent() {
-  echo '
-    <h3>Ah oh! Er gaat ergens iets mis.</h3>
-    <p>Er is bij ons een technisch probleem opgetreden. Probeer het later nog eens, of neem contact op met de beheerder van deze website.</p>
-  ';
-}
-?>
--- opdracht_3.1_mergetest/pages/order_thanks.php
+++
@@ -1,9 +0,0 @@
-<?php
-function showOrder_ThanksContent($data) {
-  echo '
-    <p class="thanksMessage">Bedankt voor uw bestelling.</p>
-    <p class="thanksMessage">Uw ordernummer is:<br><br>
-      ' . getArrayValue($data, "orderId") . '<br>
-  ';
-}
-?>
